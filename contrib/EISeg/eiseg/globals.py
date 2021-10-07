2f25d8d0162b756b0b5ed16b2a1bd0c5b5e01efd
Alex Emelyanov <alex84_84_84@mail.ru>
2021-10-07 11:06:29 +0300

    add progress, check config

--- contrib/EISeg/eiseg/globals.py
+++ contrib/EISeg/eiseg/globals.py
@@ -5,15 +5,26 @@ from supervisely_lib.io.fs import mkdir
 from ui import Ui_EISeg
 from util import MODELS
 from supervisely_lib.io.fs import download
+import requests
+import dl_progress
+
+
+def download_file_from_link(api, link, model_path, file_name, progress_message, app_logger):
+    response = requests.head(link, allow_redirects=True)
+    sizeb = int(response.headers.get('content-length', 0))
+    progress_cb = dl_progress.get_progress_cb(api, TASK_ID, progress_message, sizeb, is_size=True)
+    download(link, model_path, cache=my_app.cache, progress=progress_cb)
+    dl_progress.reset_progress(api, TASK_ID)
+    app_logger.info(f'{file_name} has been successfully downloaded')
 
 
 my_app = sly.AppService()
 api: sly.Api = my_app.public_api
 
-
 TEAM_ID = int(os.environ['context.teamId'])
 WORKSPACE_ID = int(os.environ['context.workspaceId'])
 PROJECT_ID = int(os.environ["modal.state.slyProjectId"])
+TASK_ID = int(os.environ["TASK_ID"])
 
 storage_dir = os.path.join(my_app.data_dir, "storage_dir")
 mkdir(storage_dir, True)
@@ -31,15 +42,15 @@ mkdir(cache_dir)
 param_dir = os.path.join(storage_dir, "param_dir")
 mkdir(param_dir)
 param_name = 'hrnet18_ocr64_cocolvis.pdparams'
-paramPath = os.path.join(param_dir, param_name)
+param_path = os.path.join(param_dir, param_name)
 
 modelName = 'HRNet18_OCR64'
 model_link = 'https://bj.bcebos.com/paddleseg/dygraph/interactive_segmentation/ritm/hrnet18_ocr64_cocolvis.pdparams'
-if param_name not in os.listdir(param_dir):
-    download(model_link, paramPath)
+
+download_file_from_link(api, model_link, param_path, param_name, f"Download {param_name}", my_app.logger)
 
 model = MODELS[modelName]()
-model.load_param(paramPath)
+model.load_param(param_path)
 
 prob_thresh = 0.5
 my_predictor_params = {"brs_mode": "NoBRS", "zoom_in_params": {"skip_clicks": -1, "target_size": (400, 400), "expansion_ratio": 1.4},
