d3ecbfbf8d56ad2503494d61ab115e57889d784d
cxnt <pavelbartsits@gmail.com>
2021-10-12 11:57:24 +0300

    added docker image

--- supervisely/smarttool/globals.py
+++ supervisely/smarttool/globals.py
@@ -21,6 +21,7 @@ def download_file_from_link(api, link, model_path, file_name, progress_message, 
 my_app = sly.AppService()
 api: sly.Api = my_app.public_api
 
+TASK_ID = int(os.environ["TASK_ID"])
 TEAM_ID = int(os.environ['context.teamId'])
 WORKSPACE_ID = int(os.environ['context.workspaceId'])
 TASK_ID = int(os.environ["TASK_ID"])
@@ -54,6 +55,13 @@ if param_name == 'hrnet18_ocr64_cocolvis.pdparams':
 else:
     model_name = 'HRNet18s_OCR48'
     model_link = 'https://github.com/supervisely-ecosystem/PaddleSeg/releases/download/v2.2.1/hrnet18s_ocr48_cocolvis.pdparams'
+elif param_name == 'hrnet18_ocr64_human.pdparams':
+    model_name = 'HRNet18_OCR64'
+    model_link = 'https://github.com/supervisely-ecosystem/PaddleSeg/releases/download/v2.2.1/hrnet18_ocr64_human.pdparams'
+elif param_name == 'hrnet18s_ocr48_human.pdparams':
+    model_name = 'HRNet18s_OCR48'
+    model_link = 'https://github.com/supervisely-ecosystem/PaddleSeg/releases/download/v2.2.1/hrnet18s_ocr48_human.pdparams'
+
 
 download_file_from_link(api, model_link, param_path, param_name, f"Download {param_name}", my_app.logger)
 
