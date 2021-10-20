524e0a92bf4a4d858b20ff61c48a76ab624117e5
cxnt <pavelbartsits@gmail.com>
2021-10-20 18:18:08 +0300

    load_model.py and model deploy message

--- supervisely/smarttool/globals.py
+++ supervisely/smarttool/globals.py
@@ -1,21 +1,7 @@
 import os
-import supervisely_lib as sly
 from diskcache import Cache
+import supervisely_lib as sly
 from supervisely_lib.io.fs import mkdir
-from contrib.EISeg.eiseg.ui import Ui_EISeg
-from util import MODELS
-from supervisely_lib.io.fs import download
-import requests
-import dl_progress
-
-
-def download_file_from_link(api, link, model_path, file_name, progress_message, app_logger):
-    response = requests.head(link, allow_redirects=True)
-    sizeb = int(response.headers.get('content-length', 0))
-    progress_cb = dl_progress.get_progress_cb(api, TASK_ID, progress_message, sizeb, is_size=True)
-    download(link, model_path, cache=my_app.cache, progress=progress_cb)
-    dl_progress.reset_progress(api, TASK_ID)
-    app_logger.info(f'{file_name} has been successfully downloaded')
 
 
 my_app = sly.AppService()
@@ -23,7 +9,6 @@ api: sly.Api = my_app.public_api
 
 TASK_ID = int(os.environ["TASK_ID"])
 TEAM_ID = int(os.environ['context.teamId'])
-WORKSPACE_ID = int(os.environ['context.workspaceId'])
 
 cache_item_expire_time = 600
 
@@ -42,36 +27,6 @@ mkdir(cache_dir)
 
 prob_thresh = float(os.environ['modal.state.thresh'])
 param_name = os.environ['modal.state.modelName'] + '.pdparams'
-
-model_name = None
-model_link = None
-
-
-param_path = f"/eiseg_models/{param_name}"
-if param_name == 'hrnet18_ocr64_cocolvis.pdparams':
-    model_name = 'HRNet18_OCR64'
-    model_link = 'https://github.com/supervisely-ecosystem/PaddleSeg/releases/download/v2.2.1/hrnet18_ocr64_cocolvis.pdparams'
-elif param_name == 'hrnet18s_ocr48_cocolvis.pdparams':
-    model_name = 'HRNet18s_OCR48'
-    model_link = 'https://github.com/supervisely-ecosystem/PaddleSeg/releases/download/v2.2.1/hrnet18s_ocr48_cocolvis.pdparams'
-elif param_name == 'hrnet18_ocr64_human.pdparams':
-    model_name = 'HRNet18_OCR64'
-    model_link = 'https://github.com/supervisely-ecosystem/PaddleSeg/releases/download/v2.2.1/hrnet18_ocr64_human.pdparams'
-elif param_name == 'hrnet18s_ocr48_human.pdparams':
-    model_name = 'HRNet18s_OCR48'
-    model_link = 'https://github.com/supervisely-ecosystem/PaddleSeg/releases/download/v2.2.1/hrnet18s_ocr48_human.pdparams'
-
-
-if os.path.isfile(param_path) is False:
-    param_dir = os.path.join(storage_dir, "param_dir")
-    mkdir(param_dir)
-    param_path = os.path.join(param_dir, param_name)
-    download_file_from_link(api, model_link, param_path, param_name, f"Download {param_name}", my_app.logger)
-else:
-    my_app.logger.info(f"{param_name} has been loaded from docker image")
-
-model = MODELS[model_name]()
-model.load_param(param_path)
-
-my_predictor_params = {"brs_mode": "NoBRS", "zoom_in_params": {"skip_clicks": -1, "target_size": (400, 400), "expansion_ratio": 1.4},
+my_predictor_params = {"brs_mode": "NoBRS",
+                       "zoom_in_params": {"skip_clicks": -1, "target_size": (400, 400), "expansion_ratio": 1.4},
                        "predictor_params": {"net_clicks_limit": None, "max_size": 800}}
---
+++ supervisely/smarttool/load_model.py
@@ -0,0 +1,49 @@
+import os
+import requests
+import download_progress
+import globals as g
+from contrib.EISeg.eiseg.ui import Ui_EISeg
+from util import MODELS
+import supervisely_lib as sly
+from supervisely_lib.io.fs import mkdir
+from supervisely_lib.io.fs import download
+
+
+def download_file_from_link(api, link, model_path, file_name, progress_message, app_logger):
+    response = requests.head(link, allow_redirects=True)
+    sizeb = int(response.headers.get('content-length', 0))
+    progress_cb = download_progress.get_progress_cb(api, g.TASK_ID, progress_message, sizeb, is_size=True)
+    download(link, model_path, cache=g.my_app.cache, progress=progress_cb)
+    download_progress.reset_progress(api, g.TASK_ID)
+    app_logger.info(f'{file_name} has been successfully downloaded')
+
+
+def deploy(param_name):
+    model_name = None
+    model_link = None
+
+    param_path = f"/eiseg_models/{param_name}"
+    if param_name == 'hrnet18_ocr64_cocolvis.pdparams':
+        model_name = 'HRNet18_OCR64'
+        model_link = 'https://github.com/supervisely-ecosystem/PaddleSeg/releases/download/v2.2.1/hrnet18_ocr64_cocolvis.pdparams'
+    elif param_name == 'hrnet18s_ocr48_cocolvis.pdparams':
+        model_name = 'HRNet18s_OCR48'
+        model_link = 'https://github.com/supervisely-ecosystem/PaddleSeg/releases/download/v2.2.1/hrnet18s_ocr48_cocolvis.pdparams'
+    elif param_name == 'hrnet18_ocr64_human.pdparams':
+        model_name = 'HRNet18_OCR64'
+        model_link = 'https://github.com/supervisely-ecosystem/PaddleSeg/releases/download/v2.2.1/hrnet18_ocr64_human.pdparams'
+    elif param_name == 'hrnet18s_ocr48_human.pdparams':
+        model_name = 'HRNet18s_OCR48'
+        model_link = 'https://github.com/supervisely-ecosystem/PaddleSeg/releases/download/v2.2.1/hrnet18s_ocr48_human.pdparams'
+
+    if os.path.isfile(param_path) is False:
+        param_dir = os.path.join(g.storage_dir, "param_dir")
+        mkdir(param_dir)
+        param_path = os.path.join(param_dir, param_name)
+        download_file_from_link(g.api, model_link, param_path, param_name, f"Download {param_name}", g.my_app.logger)
+    else:
+        g.my_app.logger.info(f"{param_name} has been loaded from docker image")
+
+    model = MODELS[model_name]()
+    model.load_param(param_path)
+    sly.logger.info("🟩 Model has been successfully deployed")
