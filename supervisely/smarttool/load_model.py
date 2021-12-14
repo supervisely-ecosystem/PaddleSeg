76371e0ba8334655c850778524e869a8601b6261
cxnt <pavelbartsits@gmail.com>
2021-12-14 12:15:17 +0300

    download models if needed

--- supervisely/smarttool/load_model.py
+++ supervisely/smarttool/load_model.py
@@ -1,8 +1,9 @@
+import os
 import requests
 import download_progress
 import globals as g
 import supervisely_lib as sly
-from supervisely_lib.io.fs import download
+from supervisely_lib.io.fs import download, mkdir
 from eiseg.controller import InteractiveController
 
 
@@ -16,8 +17,41 @@ def download_file_from_link(api, link, model_path, file_name, progress_message, 
 
 
 def deploy(model_name):
-    model_path = f"/eiseg_models/{model_name}/{model_name}.pdiparams"
-    # model_path = f"/home/paul/Documents/Work/Applications/PaddleSeg/supervisely/docker/eiseg_models/{model_name}/{model_name}.pdiparams" # debug
+    available_models = {
+        "static_hrnet18_ocr64_cocolvis":
+            ("https://github.com/supervisely-ecosystem/PaddleSeg/releases/download/v2.3.4/static_hrnet18_ocr64_cocolvis.pdiparams",
+             "https://github.com/supervisely-ecosystem/PaddleSeg/releases/download/v2.3.4/static_hrnet18_ocr64_cocolvis.pdmodel"),
+        "static_hrnet18s_ocr48_cocolvis":
+            ("https://github.com/supervisely-ecosystem/PaddleSeg/releases/download/v2.3.4/static_hrnet18s_ocr48_cocolvis.pdiparams",
+             "https://github.com/supervisely-ecosystem/PaddleSeg/releases/download/v2.3.4/static_hrnet18s_ocr48_cocolvis.pdmodel"),
+        "static_hrnet18_ocr64_human":
+            ("https://github.com/supervisely-ecosystem/PaddleSeg/releases/download/v2.3.4/static_hrnet18_ocr64_human.pdiparams",
+             "https://github.com/supervisely-ecosystem/PaddleSeg/releases/download/v2.3.4/static_hrnet18_ocr64_human.pdmodel"),
+        "static_hrnet18s_ocr48_human":
+            ("https://github.com/supervisely-ecosystem/PaddleSeg/releases/download/v2.3.4/static_hrnet18s_ocr48_human.pdiparams",
+             "https://github.com/supervisely-ecosystem/PaddleSeg/releases/download/v2.3.4/static_hrnet18s_ocr48_human.pdmodel"),
+        "static_edgeflow_cocolvis":
+            ("https://github.com/supervisely-ecosystem/PaddleSeg/releases/download/v2.3.4/static_edgeflow_cocolvis.pdiparams",
+             "https://github.com/supervisely-ecosystem/PaddleSeg/releases/download/v2.3.4/static_edgeflow_cocolvis.pdmodel"),
+        "static_hrnet18_ocr48_rsbuilding_instance":
+            ("https://github.com/supervisely-ecosystem/PaddleSeg/releases/download/v2.3.4/static_hrnet18_ocr48_rsbuilding_instance.pdiparams",
+             "https://github.com/supervisely-ecosystem/PaddleSeg/releases/download/v2.3.4/static_hrnet18_ocr48_rsbuilding_instance.pdmodel"),
+        "static_hrnet18s_ocr48_lits":
+            ("https://github.com/supervisely-ecosystem/PaddleSeg/releases/download/v2.3.4/static_hrnet18s_ocr48_lits.pdiparams",
+             "https://github.com/supervisely-ecosystem/PaddleSeg/releases/download/v2.3.4/static_edgeflow_cocolvis.pdmodel")
+    }
+
+    pdiparams_link, pdmodel_link = available_models[model_name]
+    param_path = f"/eiseg_models/{model_name}/{model_name}.pdiparams"
+    if os.path.isfile(param_path) is False:
+        model_dir = os.path.join(g.work_dir, "eiseg_models", model_name)
+        mkdir(model_dir)
+        param_path = os.path.join(model_dir, f"{model_name}.pdiparams")
+        model_path = os.path.join(model_dir, f"{model_name}.pdmodel")
+        download_file_from_link(g.api, pdiparams_link, param_path, model_name, f"Download {model_name}.pdiparams", g.my_app.logger)
+        download_file_from_link(g.api, pdmodel_link, model_path, model_name, f"Download {model_name}.pdmodel", g.my_app.logger)
+    else:
+        g.my_app.logger.info(f"{model_name} has been loaded from docker image")
 
     if model_name == "static_edgeflow_cocolvis":
         with_mask = False
@@ -40,5 +74,5 @@ def deploy(model_name):
     }
 
     g.CONTROLLER = InteractiveController(predictor_params, prob_thresh=g.PROB_THRESH)
-    g.CONTROLLER.setModel(model_path)
+    g.CONTROLLER.setModel(param_path)
     sly.logger.info("🟩 Model has been successfully deployed")
